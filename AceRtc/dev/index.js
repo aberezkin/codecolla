cf522d183bb6a25de028a5d6611a95ca4ca99303
kofes <turnever@gmail.com>
2017-10-14 14:57:09 +1000

    AceEditor init

--- AceRtc/dev/index.js
+++ AceRtc/dev/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App' ;
+import AceEditor from 'react-ace';
 
+import 'brace/mode/java';
+import 'brace/theme/github';
+
+function onChange(newValue) {
+    console.log('change',newValue);
+}
 ReactDOM.render(
-    <App />,
+    <AceEditor
+        mode="java"
+        theme="github"
+        onChange={onChange}
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{$blockScrolling: true}}
+    />,
     document.getElementById('root')
 );
