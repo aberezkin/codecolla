62d4c9a4b7e39fcdbe10b394478bfdd22c779936
kofes <turnever@gmail.com>
2017-10-21 13:24:07 +1000

    AceRtc moved to src/

--- src/dev/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/StatusBar/StatusBar';
-import './index.styl';
-
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-);
