0eac9fb9675c1b71f4effb2c34b56982ac1281a2
Konstantin S <turnever@gmail.com>
2017-10-31 17:29:34 +1000

    StatusBar: import selectors

--- src/dev/components/StatusBar/StatusBar.js
+++ src/dev/components/StatusBar/StatusBar.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react'
 import './StatusBar.styl';
+import LineSeparatorSelector from './LineSeparatorSelector/LineSeparatorSelector';
+import LanguageSelector from './LanguageSelector/LanguageSelector';
+import EncodingSelector from './EncodingSelector/EncodingSelector';
+import ColorSchemeSelector from './ColorSchemeSelector/ColorSchemeSelector';
 
 class StatusBar extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
-            <div className="StatusBar">
+            <div className="StatusBar" style={{width: this.props.width,
+                                               height: this.props.height}}>
                 <div className="Left">
                 </div>
                 <div className="Right">
+                    <LineSeparatorSelector />
+                    <EncodingSelector />
+                    <LanguageSelector />
+                    <ColorSchemeSelector />
                 </div>
             </div>
         )
     }
 }
+
+StatusBar.defaultProps = {
+    width: '100%',
+    height: '20px',
+};
 
 export default StatusBar;
