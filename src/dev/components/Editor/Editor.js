4d1b05229409d205eb267c9183454e7cfe9d650a
Konstantin S <turnever@gmail.com>
2017-11-05 22:49:53 +1000

    Editor: new props & style-file addition
    Editor has default props 'mode' & 'theme'

--- src/dev/components/Editor/Editor.js
+++ src/dev/components/Editor/Editor.js
@@ -1,27 +1,38 @@
 import React, {Component} from 'react';
 import brace from 'brace';
 import AceEditor from 'react-ace';
+import './Editor.styl';
 
 import 'brace/mode/java';
 import 'brace/theme/github';
 
-function onChange(newValue) {
-    console.log('change',newValue);
+function onChange(value) {
+    console.log('change:\n', value);
 }
 
 class Editor extends Component {
+    constructor(props) {
+        super(props);
+    }
     // Render editor
     render() {
         return (
             <AceEditor
-            mode="java"
-            theme="github"
+            mode={this.props.mode}
+            theme={this.props.theme}
+            width={'100%'}
+            height={'100%'}
             onChange={onChange}
             name="UNIQUE_ID_OF_DIV"
-            editorProps={{$blockScrolling: true}}
+            editorProps={{$blockScrolling: 'Infinity'}}
             />
         );
     }
 }
+
+Editor.defaultProps = {
+    mode : 'text',
+    theme: 'github',
+};
 
 export default Editor;
