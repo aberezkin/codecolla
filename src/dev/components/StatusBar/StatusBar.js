96adb938998ebb81e6f85a1b4f4542e212b50d03
Konstantin S <turnever@gmail.com>
2017-11-05 22:46:06 +1000

    StatusBar:  added new props
    added static method 'name', width & height moved to props.style, added onChange & init_value for all sections of status bar

--- src/dev/components/StatusBar/StatusBar.js
+++ src/dev/components/StatusBar/StatusBar.js
@@ -5,21 +5,37 @@ import LanguageSelector from './LanguageSelector/LanguageSelector';
 import EncodingSelector from './EncodingSelector/EncodingSelector';
 import ColorSchemeSelector from './ColorSchemeSelector/ColorSchemeSelector';
 
+//onChange(event, key, value)
+
 class StatusBar extends Component {
     constructor(props) {
         super(props);
     }
+    static name() {
+        return 'StatusBar';
+    }
     render() {
         return (
-            <div className="StatusBar" style={{width: this.props.width,
-                                               height: this.props.height}}>
+            <div className={StatusBar.name()} style={this.props.style}>
                 <div className="Left">
                 </div>
                 <div className="Right">
-                    <LineSeparatorSelector />
-                    <EncodingSelector />
-                    <LanguageSelector />
-                    <ColorSchemeSelector />
+                    <LineSeparatorSelector
+                        selectedOption={this.props.valueEnding}
+                        onChange={this.props.onChangeEnding}
+                    />
+                    <EncodingSelector
+                        selectedOption={this.props.valueEncoding}
+                        onChange={this.props.onChangeEncoding}
+                    />
+                    <LanguageSelector
+                        selectedOption={this.props.valueLanguage}
+                        onChange={this.props.onChangeLanguage}
+                    />
+                    <ColorSchemeSelector
+                        selectedOption={this.props.valueTheme}
+                        onChange={this.props.onChangeTheme}
+                    />
                 </div>
             </div>
         )
@@ -27,8 +43,26 @@ class StatusBar extends Component {
 }
 
 StatusBar.defaultProps = {
-    width: '100%',
-    height: '20px',
+    style: {
+        width: '100%',
+        height: '20px',
+    },
+    valueEnding : 'LF',
+    onChangeEnding : (event, key, value) => {
+        console.log(key, value);
+    },
+    valueEncoding : 'UTF-8',
+    onChangeEncoding : (event, key, value) => {
+        console.log(key, value);
+    },
+    valueLanguage : 'text',
+    onChangeLanguage : (event, key, value) => {
+        console.log(key, value);
+    },
+    valueTheme : 'github',
+    onChangeTheme : (event, key, value) => {
+        console.log(key, value);
+    },
 };
 
 export default StatusBar;
