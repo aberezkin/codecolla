63357ebcafeeaffd1100a529d09314be38626eaa
Konstantin S <turnever@gmail.com>
2017-12-12 00:36:29 +1000

    fix getHeight-helper

--- src/utilities/Helpers.js
+++ src/utilities/Helpers.js
@@ -1,11 +1,8 @@
 import {Map} from 'immutable';
 
 export function getPageHeight () {
-    let body = document.body,
-        html = document.documentElement;
-
-    return Math.max(body.scrollHeight, body.offsetHeight,
-        html.clientHeight, html.scrollHeight, html.offsetHeight);
+    let html = document.documentElement;
+    return html.clientHeight;
 }
 
 export function generateLineId() {
