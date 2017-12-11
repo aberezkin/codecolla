8859e4d89c15ee2b5a0924368d2c9c55bf22ecfb
Arkadii Berezkin <arkm4n@gmail.com>
2017-12-11 13:52:54 +1000

    Add tests for chat reducer

---
+++ src/reducers/chat.test.js
@@ -0,0 +1,33 @@
+import reducer from './chat';
+import {ADD_MESSAGE} from "../actions/index";
+
+const messages = reducer.__get__('messages');
+
+describe('messages reducer', () => {
+    let state = [
+        {author: 'me', content: 'Hello!', date: new Date().toLocaleTimeString()},
+        {author: 'you', content: 'Hi!', date: new Date().toLocaleTimeString()},
+        {author: 'me', content: 'How are you?', date: new Date().toLocaleTimeString()}
+    ];
+
+    it('should return default value', () => {
+        expect(messages(undefined, {})).toEqual([]);
+    });
+
+    it(`should handle ${ADD_MESSAGE} action`, () => {
+        let message = {
+            author: 'you',
+            content: 'Fine, let\'s code!',
+            date: new Date().toLocaleTimeString()
+        };
+
+        let newState = messages(state, {type: ADD_MESSAGE, payload: message});
+        expect(newState.length).toBe(state.length + 1);
+        expect(newState[newState.length - 1]).toEqual(message);
+    });
+
+    it('should return same state on meaningless action', () => {
+        expect(messages(state, {})).toBe(state);
+    })
+});
+
