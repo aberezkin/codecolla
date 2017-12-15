820b0ba1205be16d10cc97ebc129cb651ac8d166
Konstantin S <turnever@gmail.com>
2017-12-16 03:21:53 +1000

    make MenuItem work

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -6,22 +6,49 @@ class MenuBar extends Component {
     constructor(props) {
         super(props);
         this.renderMenuItem = this.renderMenuItem.bind(this);
-        this.state = {
-            isActive: false,
-        };
+        this.bindSetInactiveHandler = this.bindSetInactiveHandler.bind(this);
+        this.unbindSetInactiveHandler = this.unbindSetInactiveHandler.bind(this);
+        this.onClick = this.onClick.bind(this);
+        this.handleDocumentClick = this.handleDocumentClick.bind(this);
+        this.state = {isActive: false};
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.isActive && !prevState.isActive) {
+            this.bindSetInactiveHandler()
+        }
+
+        else if (prevState.isActive && !this.state.isActive) {
+            this.unbindSetInactiveHandler()
+        }
     }
 
     renderMenuItem(child) {
         return React.cloneElement(child, {
             isMenuBarActive: this.state.isActive, //is MenuBar 'opened'
-            isMenuItemRoot: true,                 //is element on top
+            isTopLevel: true,                     //is element on top
             onSelect: this.props.onSelect,        //callback forall commands
         });
     }
 
+    bindSetInactiveHandler() {
+        document.addEventListener('click', this.handleDocumentClick, false);
+    }
+
+    unbindSetInactiveHandler() {
+        document.removeEventListener('click', this.handleDocumentClick);
+    }
+
+    handleDocumentClick(event) {
+        this.setState({isActive: false});
+    }
+
+    onClick(event) {
+        this.setState({isActive: !this.state.isActive});
+    }
+
     render() {
         return (
-            <ul className={`${MENU_BAR_CLASSNAME}`}>
+            <ul className={`${MENU_BAR_CLASSNAME}`} onClick={this.onClick}>
                 {React.Children.map(this.props.children, this.renderMenuItem)}
             </ul>
         )
