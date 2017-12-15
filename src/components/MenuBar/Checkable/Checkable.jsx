08447b1e52a2185112e176e8a18713732573c3b8
Konstantin S <turnever@gmail.com>
2017-12-16 06:06:03 +1000

    create Checkable-component

---
+++ src/components/MenuBar/Checkable/Checkable.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+
+class Checkable extends Component {
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+        this.onSelect = this.onSelect.bind(this);
+    }
+
+    onClick(event) {
+        event.preventDefault();
+        this.props.onSelect(this.props.command);
+    }
+
+    render() {
+        return (
+            <li className='Checkable'>
+                <span className='title' onClick={this.onClick}>{this.props.title}</span>
+            </li>
+        )
+    }
+
+    onSelect(command) {
+        this.props.onSelect(command);
+    }
+}
+
+Checkable.defaultProps = {
+    command: '',
+};
+
+Checkable.propTypes = {
+    title: PropTypes.string.isRequired,
+};
+
+export default Checkable;
