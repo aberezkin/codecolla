8c4d9c406ef9472f2ceed6a78cee29f464771ecf
Arkadii Berezkin <arkm4n@gmail.com>
2017-12-21 00:48:11 +1000

    Add fade out animation on connection route

--- src/components/Branching/Branching.jsx
+++ src/components/Branching/Branching.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { AnimatedSwitch } from 'react-router-transition';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import RouteConnector from "../RouteConnector";
 import App from "../App";
@@ -16,12 +17,19 @@ export default class Branching extends Component {
 
     render() {
         return (
-            <Router>
-                <div>
-                    <Route path="/connect/:id" component={RouteConnector}/>
-                    <Route exact path="/" component={App} />
-                </div>
-            </Router>
+            <div>
+                <Router>
+                    <AnimatedSwitch
+                        atEnter={{ opacity: 0 }}
+                        atLeave={{ opacity: 0 }}
+                        atActive={{ opacity: 1 }}
+                        className="switch-wrapper"
+                    >
+                        <Route exact path="/" component={App} />
+                        <Route path="/connect/:id" component={RouteConnector}/>
+                    </AnimatedSwitch>
+                </Router>
+            </div>
         )
     }
 }
