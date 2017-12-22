d3c59f63d231edeb318250671081be85981e612f
Konstantin S <turnever@gmail.com>
2017-12-22 23:12:02 +1000

    add new actions for Menu-component

--- src/actions/index.js
+++ src/actions/index.js
@@ -113,3 +113,9 @@ export function handleMenuCommand(command) {
 
 export const TOGGLE_STATUS_BAR = 'TOGGLE STATUS BAR';
 export const TOGGLE_CHAT = 'TOGGLE CHAT';
+export const CREATE_NEW = 'CREATE NEW';
+export const OPEN_SMTH = 'OPEN SMTH';
+export const OPEN_URL = 'OPEN URL';
+export const SAVE_AS = 'SAVE AS';
+export const SAVE_ALL = 'SAVE ALL';
+export const OPEN_SETTINGS = 'OPEN SETTINGS';
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { MenuBar, Checkable, MenuItem, Separator, SubMenu } from '../MenuBar';
-import { TOGGLE_STATUS_BAR, TOGGLE_CHAT } from '../../actions';
+import {
+    TOGGLE_STATUS_BAR,
+    TOGGLE_CHAT,
+    CREATE_NEW,
+    OPEN_SMTH,
+    OPEN_URL,
+    SAVE_AS,
+    SAVE_ALL,
+    OPEN_SETTINGS,
+} from '../../actions';
 import './Menu.styl';
 
 class Menu extends Component {
@@ -21,14 +30,14 @@ class Menu extends Component {
                 <MenuBar onSelect={this.onMenuOptionHandler} style={this.props.style}>
                     <MenuItem  title={'File'}>
                         <SubMenu>
-                            <MenuItem title={'New'} command={'menu-new'}/>
-                            <MenuItem title={'Open...'} command={'menu-open'}/>
-                            <MenuItem title={'Open URL'} command={'menu-open-url'}/>
+                            <MenuItem title={'New'} command={CREATE_NEW}/>
+                            <MenuItem title={'Open...'} command={OPEN_SMTH}/>
+                            <MenuItem title={'Open URL'} command={OPEN_URL}/>
                             <Separator />
-                            <MenuItem title={'Save as...'} command={'menu-save-as'}/>
-                            <MenuItem title={'Save all'} command={'menu-save-all'}/>
+                            <MenuItem title={'Save as...'} command={SAVE_AS}/>
+                            <MenuItem title={'Save all'} command={SAVE_ALL}/>
                             <Separator />
-                            <MenuItem title={'Settings'} command={'menu-settings'}/>
+                            <MenuItem title={'Settings'} command={OPEN_SETTINGS}/>
                         </SubMenu>
                     </MenuItem>
                     <MenuItem  title={'Edit'}>
