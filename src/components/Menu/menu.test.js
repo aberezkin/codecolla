587833a46d112b63acdf9c9b9cb498e76332e956
Konstantin S <turnever@gmail.com>
2017-12-22 23:02:01 +1000

    add some tests for Menu-component

---
+++ src/components/Menu/menu.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { MenuBar, Checkable, MenuItem, Separator, SubMenu } from '../MenuBar';
+
+const setup = propChildren => {
+    const props = {
+        children: [<MenuItem key={0} title={'Test'} command={'test'} />],
+        onSelect: jest.fn(),
+        ...propChildren,
+    };
+
+    const wrapper = mount((
+        <MenuBar onSelect={props.onSelect}>
+            {props.children}
+        </MenuBar>
+    ));
+
+    return {
+        onSelect: props.onSelect,
+        wrapper,
+    };
+};
+
+describe('menu component', () => {
+    it('render MenuBar', () => {
+        const { wrapper } = setup();
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('check rendering of MenuItem', () => {
+        const { wrapper } = setup();
+        expect(wrapper.contains([<MenuItem title={'Test'} command={'test'} />])).toBeTruthy();
+    });
+
+    it('should call onSelect when click MenuItem "test"', () => {
+        const { onSelect, wrapper } = setup();
+        wrapper.find('.MenuItem .title').simulate('click');
+        expect(onSelect).toHaveBeenCalled();
+    });
+});
