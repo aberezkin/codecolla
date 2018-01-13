173d97ffb7756ff7138d23ec9aceecad74368dd0
Arkadii Berezkin <arkm4n@gmail.com>
2018-01-13 13:43:03 +1000

    Revert "Make rebase great again!"

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { insertEvent, removeEvent, setIsTransferAllowed, moveCursor } from '../../actions/index';
+import { insertEvent, removeEvent, setIsTransferAllowed } from '../../actions/index';
 import Editor from './Editor';
 
 const mapStateToProps = state => ({
@@ -8,23 +8,12 @@ const mapStateToProps = state => ({
     isTransferAllowed: state.isTransferAllowed,
     theme: state.preferences.theme,
     language: state.preferences.editor.language,
-    cursors: state.cursors,
-    peerId: state.peers.id,
 });
 
 const mapDispatchToProps = dispatch => ({
     allowEventTransfer: flag => dispatch(setIsTransferAllowed(flag)),
     onInsert: e => dispatch(insertEvent(e)),
     onRemove: e => dispatch(removeEvent(e)),
-    moveCursor: id => pos => dispatch(moveCursor(id, pos)),
 });
 
-const mergeProps = (stateProps, dispatchProps, ownProps) => ({
-    ...ownProps,
-    ...stateProps,
-    ...dispatchProps,
-    peerId: undefined,
-    moveCursor: dispatchProps.moveCursor(stateProps.peerId),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps, mergeProps, { withRef: true })(Editor);
+export default connect(mapStateToProps, mapDispatchToProps, null, { withRef: true })(Editor);
