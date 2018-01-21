e127713c15a438577747a47e4c33f8d1475142c3
Даниил Глущенко <J-CITY>
2018-01-21 22:37:39 +1000

    Do not insert exist peer

--- src/reducers/peer.js
+++ src/reducers/peer.js
@@ -1,13 +1,18 @@
 import { combineReducers } from 'redux';
 import { ADD_PEER, REMOVE_PEER, SET_PEER_ID } from '../actions/index';
 import { generateSetterReducer } from '../utilities/Helpers';
+import peer from '../middleware/peer';
 
 const idReducer = generateSetterReducer(SET_PEER_ID, '');
 
 function connectionsReducer(state = [], action) {
     switch (action.type) {
-        case ADD_PEER:
+        case ADD_PEER: {
+            if (state.find(peer => peer == action.payload)) {
+                return state;
+            }
             return [...state, action.payload];
+        }
         case REMOVE_PEER:
             return state.filter(conn => conn.peer !== action.payload.peer);
         default:
