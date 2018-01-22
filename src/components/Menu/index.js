fdbb73c76ac1ca488a4ab73861014ee03569f944
Konstantin S <turnever@gmail.com>
2018-01-22 12:22:23 +1000

    customize Menu by adding(and setting) checkboxes to Checkable component & props.style to MenuItem component

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,10 +1,16 @@
 import { connect } from 'react-redux';
 import Menu from './Menu';
 import { createSimpleAction } from '../../actions/index';
+import PropTypes from "prop-types";
+
+const mapStateToProps = state => ({
+    isStatusBarVisible : state.preferences.isStatusBarVisible,
+    isChatVisible: state.preferences.isChatVisible,
+});
 
 // TODO: move this to individual selectors
 const mapDispatchToProps = dispatch => ({
     onMenuOptionHandler: command => dispatch(createSimpleAction(command)),
 });
 
-export default connect(null, mapDispatchToProps)(Menu);
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
